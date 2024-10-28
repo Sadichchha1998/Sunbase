2b5fa21df5cb028ef29bbde94950a616dfb5b763
Sadichchha Naiknavare <110052591+Sadichchha1998@users.noreply.github.com>
2024-10-28 15:43:38 +0530

    Update Customer.java
    Exception Added

--- Sunbase/SB201F/Sunbase/Sunbase/src/main/java/com/sunbase/model/Customer.java
+++ Sunbase/SB201F/Sunbase/Sunbase/src/main/java/com/sunbase/model/Customer.java
@@ -19,14 +19,34 @@ public class Customer {
  @GeneratedValue(strategy = GenerationType.IDENTITY)
  private Long id;
 
- private String firstName;
- private String lastName;
- private String street;
- private String address;
- private String city;
- private String state;
- private String email;
- private String phone;
-
- // Getters and Setters
+@NotEmpty(message = "first name should be not Empty and not null! ")
+	@NotNull
+	@JsonProperty("first_name")
+	private String first_name;
+ 
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null! ")
+	@JsonProperty("last_name")
+	private String last_name;
+ 
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null!")
+	private String street;
+ 
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null!")
+	private String address;
+ 
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null!")
+	private String city;
+ 
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null!")
+	private String state;
+	@Email(message = "please write email format and not null!")
+	private String email;
+	@NotNull
+	@NotEmpty(message = "Last name should be not Empty and not null!")
+	private String phone;
 }
